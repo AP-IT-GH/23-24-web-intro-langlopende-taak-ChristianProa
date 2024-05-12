a8c7c789449652fc97c05dc53c26cb3550dd02f6
ChristianProa <christoforoo@gmail.com>
2024-05-12 11:03:27 +0200

    update

--- js/mapScript.js
+++ js/mapScript.js
@@ -9,11 +9,18 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+let customIcon = L.icon({
+    iconUrl: '/assests/icons/Logo.png', // URL de tu logo
+    iconSize: [40, 40], // Tamaño del ícono, ajusta según sea necesario
+    iconAnchor: [20, 40], // Punto del ícono que corresponderá a la ubicación del marcador
+    popupAnchor: [0, -40] // Punto desde donde se abrirá el popup, si es necesario ajustarlo
+});
+
 
 // bepaal de rechthoek rondom het gebouw van AP
 let bounds = [[51.23041, 4.4155], [51.22991, 4.41675]];
 // kleur de rechthoek in met de rode AP-kleur
 L.rectangle(bounds, {color: "#e60005", weight: 1}).addTo(map);
 // plaats een marker met als tekst "AP-Hogeschool" en eronder "Ellermanstraat 33"
-let apMarker = L.marker([51.23009, 4.41616]).addTo(map);
-apMarker.bindPopup("<b>AP-Hogeschool</b><br>Ellermanstraat 33").openPopup();
+let apMarker = L.marker([51.23009, 4.41616],{icon: customIcon}).addTo(map);
+apMarker.bindPopup("<b>Netwerk-Protocollen</b><br>Ellermanstraat 33").openPopup();
